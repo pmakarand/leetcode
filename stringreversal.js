13d6e3d2f436373e35a8515bf5743705546e41d3
Makarand Pathak <makarand@Makarands-MBP.lan>
2020-02-10 21:39:03 -0800

    added comment

--- stringreversal.js
+++ stringreversal.js
@@ -1,4 +1,7 @@
-reverse("log")
+// Write a javascript function named reverse which takes a string argument and returns the reversed string.
+
+reverse("testeng")
+
 function reverse(str)
 {
   var rev;
@@ -8,8 +11,30 @@ function reverse(str)
   { 
    rev2.push((rev.pop()));
   }
-  console.log(rev2.join(""));
   return rev2.join("");
-  
+}
+
+// Actual solution
+
+function str_reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+//Solution 3
+function str_reverse2(str)
+{
+  let reversed ='';
+    for (let character of str)
+    {
+     reversed = character+reversed
+    }
+  return reversed;
+}
+
+//Solution 4 - reduce
+
+function str_reverse3(str)
+{
+   return str.split('').reduce((reversed,character)=>{return reversed=character+reversed},'')
 }
  
